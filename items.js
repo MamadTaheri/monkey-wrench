a1546bddee9ff86e85bfd365b6ebbc0e2b3e3745
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-04 12:17:44 +0330

    add items1 to repository

---
+++ items.js
@@ -0,0 +1,21 @@
+const items1 = [
+    {
+      title: "What is React?",
+      content: "React is a front end javascript framework",
+    },
+    {
+      title: "Why use React?",
+      content: "React is a favorite JS library among engineers",
+    },
+    {
+      title: "How do you use React?",
+      content: "You use React by creating components",
+    },
+  ];
+
+//   ES5 export
+module.exports = {items1}
+
+
+//   ES6 export
+export {items1};
