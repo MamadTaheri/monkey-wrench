645e970bfa1ec4e81cde096ea81ffbdf26378774
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-04 20:51:24 +0330

    add languages data

---
+++ data/languages.js
@@ -0,0 +1,768 @@
+const languages = [
+  {
+    id: 00,
+    label: "Afar",
+    value: "aa",
+  },
+  {
+    id: 00,
+    label: "Abkhazian",
+    value: "ab",
+  },
+  {
+    id: 00,
+    label: "Avestan",
+    value: "ae",
+  },
+  {
+    id: 00,
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    id: 00,
+    label: "Akan",
+    value: "ak",
+  },
+  {
+    id: 00,
+    label: "Amharic",
+    value: "am",
+  },
+  {
+    id: 00,
+    label: "Aragonese",
+    value: "an",
+  },
+  {
+    id: 00,
+    label: "Arabic",
+    value: "ar",
+  },
+  {
+    id: 00,
+    label: "Assamese",
+    value: "as",
+  },
+  {
+    id: 00,
+    label: "Avaric",
+    value: "av",
+  },
+  {
+    id: 00,
+    label: "Aymara",
+    value: "ay",
+  },
+  {
+    id: 00,
+    label: "Azerbaijani",
+    value: "az",
+  },
+  {
+    id: 00,
+    label: "Bashkir",
+    value: "ba",
+  },
+  {
+    id: 00,
+    label: "Belarusian",
+    value: "be",
+  },
+  {
+    id: 00,
+    label: "Bulgarian",
+    value: "bg",
+  },
+  {
+    id: 00,
+    label: "Bihari languages",
+    value: "bh",
+  },
+  {
+    id: 00,
+    label: "Bislama",
+    value: "bi",
+  },
+  {
+    id: 00,
+    label: "Bambara",
+    value: "bm",
+  },
+  {
+    id: 00,
+    label: "Bengali",
+    value: "bn",
+  },
+  {
+    id: 00,
+    label: "Tibetan",
+    value: "bo",
+  },
+  {
+    id: 00,
+    label: "Breton",
+    value: "br",
+  },
+  {
+    id: 00,
+    label: "Bosnian",
+    value: "bs",
+  },
+  {
+    id: 00,
+    label: "Catalan",
+    value: "ca",
+  },
+  {
+    id: 00,
+    label: "Chechen",
+    value: "ce",
+  },
+  {
+    id: 00,
+    label: "Chamorro",
+    value: "ch",
+  },
+  {
+    id: 00,
+    label: "Corsican",
+    value: "co",
+  },
+  {
+    id: 00,
+    label: "Cree",
+    value: "cr",
+  },
+  {
+    id: 00,
+    label: "Czech",
+    value: "cs",
+  },
+  {
+    id: 00,
+    label: "Church Slavic",
+    value: "cu",
+  },
+  {
+    id: 00,
+    label: "Chuvash",
+    value: "cv",
+  },
+  {
+    id: 00,
+    label: "Welsh",
+    value: "cy",
+  },
+  {
+    id: 00,
+    label: "Danish",
+    value: "da",
+  },
+  {
+    id: 00,
+    label: "German",
+    value: "de",
+  },
+  {
+    id: 00,
+    label: "Divehi",
+    value: "dv",
+  },
+  {
+    id: 00,
+    label: "Dzongkha",
+    value: "dz",
+  },
+  {
+    id: 00,
+    label: "Ewe",
+    value: "ee",
+  },
+  {
+    id: 00,
+    label: "Greek, Modern (1453-)",
+    value: "el",
+  },
+  {
+    id: 00,
+    label: "English",
+    value: "en",
+  },
+  {
+    id: 00,
+    label: "Esperanto",
+    value: "eo",
+  },
+  {
+    id: 00,
+    label: "Spanish",
+    value: "es",
+  },
+  {
+    id: 00,
+    label: "Estonian",
+    value: "et",
+  },
+  {
+    id: 00,
+    label: "Basque",
+    value: "eu",
+  },
+  {
+    id: 00,
+    label: "Persian",
+    value: "fa",
+  },
+  {
+    id: 00,
+    label: "Fulah",
+    value: "ff",
+  },
+  {
+    id: 00,
+    label: "Finnish",
+    value: "fi",
+  },
+  {
+    id: 00,
+    label: "Fijian",
+    value: "fj",
+  },
+  {
+    id: 00,
+    label: "Faroese",
+    value: "fo",
+  },
+  {
+    id: 00,
+    label: "French",
+    value: "fr",
+  },
+  {
+    id: 00,
+    label: "Western Frisian",
+    value: "fy",
+  },
+  {
+    id: 00,
+    label: "Irish",
+    value: "ga",
+  },
+  {
+    id: 00,
+    label: "Gaelic",
+    value: "gd",
+  },
+  {
+    id: 00,
+    label: "Galician",
+    value: "gl",
+  },
+  {
+    id: 00,
+    label: "Guarani",
+    value: "gn",
+  },
+  {
+    id: 00,
+    label: "Gujarati",
+    value: "gu",
+  },
+  {
+    id: 00,
+    label: "Manx",
+    value: "gv",
+  },
+  {
+    id: 00,
+    label: "Hausa",
+    value: "ha",
+  },
+  {
+    id: 00,
+    label: "Hebrew",
+    value: "he",
+  },
+  {
+    id: 00,
+    label: "Hindi",
+    value: "hi",
+  },
+  {
+    id: 00,
+    label: "Hiri Motu",
+    value: "ho",
+  },
+  {
+    id: 00,
+    label: "Croatian",
+    value: "hr",
+  },
+  {
+    id: 00,
+    label: "Haitian",
+    value: "ht",
+  },
+  {
+    id: 00,
+    label: "Hungarian",
+    value: "hu",
+  },
+  {
+    id: 00,
+    label: "Armenian",
+    value: "hy",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "Herero",
+    value: "hz",
+  },
+  {
+    id: 00,
+    label: "Interlingua (International Auxiliary Language Association)",
+    value: "ia",
+  },
+  {
+    id: 00,
+    label: "Indonesian",
+    value: "id",
+  },
+  {
+    id: 00,
+    label: "Interlingue",
+    value: "ie",
+  },
+  {
+    id: 00,
+    label: "Igbo",
+    value: "ig",
+  },
+  {
+    id: 00,
+    label: "Sichuan Yi",
+    value: "ii",
+  },
+  {
+    id: 00,
+    label: "Inupiaq",
+    value: "ik",
+  },
+  {
+    id: 00,
+    label: "Ido",
+    value: "io",
+  },
+  {
+    id: 00,
+    label: "Icelandic",
+    value: "is",
+  },
+  {
+    id: 00,
+    label: "Italian",
+    value: "it",
+  },
+  {
+    id: 00,
+    label: "Inuktitut",
+    value: "iu",
+  },
+  {
+    id: 00,
+    label: "Japanese",
+    value: "ja",
+  },
+  {
+    id: 00,
+    label: "Javanese",
+    value: "jv",
+  },
+  {
+    id: 00,
+    label: "Georgian",
+    value: "ka",
+  },
+  {
+    id: 00,
+    label: "Kongo",
+    value: "kg",
+  },
+  {
+    id: 00,
+    label: "Kikuyu",
+    value: "ki",
+  },
+  {
+    id: 00,
+    label: "Kuanyama",
+    value: "kj",
+  },
+  {
+    id: 00,
+    label: "Kazakh",
+    value: "kk",
+  },
+  {
+    id: 00,
+    label: "Kalaallisut",
+    value: "kl",
+  },
+  {
+    id: 00,
+    label: "Khmer",
+    value: "km",
+  },
+  {
+    id: 00,
+    label: "Kannada",
+    value: "kn",
+  },
+  {
+    id: 00,
+    label: "Korean",
+    value: "ko",
+  },
+  {
+    id: 00,
+    label: "Kanuri",
+    value: "kr",
+  },
+  {
+    id: 00,
+    label: "Kashmiri",
+    value: "ks",
+  },
+  {
+    id: 00,
+    label: "Kurdish",
+    value: "ku",
+  },
+  {
+    id: 00,
+    label: "Komi",
+    value: "kv",
+  },
+  {
+    id: 00,
+    label: "Cornish",
+    value: "kw",
+  },
+  {
+    id: 00,
+    label: "Kirghiz",
+    value: "ky",
+  },
+  {
+    id: 00,
+    label: "Latin",
+    value: "la",
+  },
+  {
+    id: 00,
+    label: "Luxembourgish",
+    value: "lb",
+  },
+  {
+    id: 00,
+    label: "Ganda",
+    value: "lg",
+  },
+  {
+    id: 00,
+    label: "Limburgan",
+    value: "li",
+  },
+  {
+    id: 00,
+    label: "Lingala",
+    value: "ln",
+  },
+  {
+    id: 00,
+    label: "Lao",
+    value: "lo",
+  },
+  {
+    id: 00,
+    label: "Lithuanian",
+    value: "lt",
+  },
+  {
+    id: 00,
+    label: "Luba-Katanga",
+    value: "lu",
+  },
+  {
+    id: 00,
+    label: "Latvian",
+    value: "lv",
+  },
+  {
+    id: 00,
+    label: "Malagasy",
+    value: "mg",
+  },
+  {
+    id: 00,
+    label: "Marshallese",
+    value: "mh",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+  {
+    id: 00,
+    label: "",
+    value: "",
+  },
+];
+
+//   ES5 export
+module.exports = { languages };
+
+//   ES6 export
+export { languages };
