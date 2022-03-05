2eccd83d48046509987d038dab6ca05afd116fff
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-05 23:12:31 +0330

    add faMonths

---
+++ data/months.js
@@ -0,0 +1,22 @@
+const faMonths = [
+    'فروردین',
+    'اردیبهشت',
+    'خرداد',
+    'تیر',
+    'مرداد',
+    'شهریور',
+    'مهر',
+    'آبان',
+    'آذر',
+    'دی',
+    'بهمن',
+    'اسفند',
+]
+     
+
+//   ES5 export
+module.exports = {faMonths}
+
+
+//   ES6 export
+export {faMonths};
