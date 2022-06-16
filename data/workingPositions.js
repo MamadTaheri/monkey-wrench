20a1395a80a83ca878b76ccf0e1aa3a2c9a40e03
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-16 13:15:04 +0430

    add workingPositions data

---
+++ data/workingPositions.js
@@ -0,0 +1,15 @@
+const workingPositions = {
+    JUNIOR = 'junior',
+    PROGRAMMER = 'programmer',
+    ENGINNER = 'enginner',
+    EXPERT = 'expert',
+    MANAGER = 'manager'
+}
+
+
+//   ES5 export
+module.exports = {workingPositions}
+
+
+//   ES6 export
+export {workingPositions};
